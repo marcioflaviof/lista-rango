77d52e31f3533468718fca139a367769921d1773
Marcio Flavio <mflaviof1995@gmail.com>
2022-02-09 15:38:16 -0300

    Component Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Nav } from "./atoms/Nav";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Nav />
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/atoms/Nav/index.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  content: " ";
+  background-color: #009ca3;
+  width: 100vw;
+  height: 60px;
+  box-shadow: 0px 4px 8px #00000029;
+`;
+
+const Nav = () => {
+  return <Container />;
+};
+
+export { Nav };
