98c16bb668de9c50bc213fb979cc64a2c1dfae2c
Marcio Flavio <mflaviof1995@gmail.com>
2022-02-11 12:25:48 -0300

    Setting routes to object

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,11 +3,16 @@ import { Routes, Route } from "react-router";
 import { Home } from "./components/pages/Home";
 import { Restaurant } from "./components/pages/Restaurant";
 
+const ROUTES = {
+  HOME: "/",
+  RESTAURANT: "/restaurant/:id",
+};
+
 export default function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/restaurant/:id" element={<Restaurant />} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.RESTAURANT} element={<Restaurant />} />
     </Routes>
   );
 }
